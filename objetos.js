2296d0dad556b1ada40259e213bef37dba312bf1
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-11-01 10:41:17 -0300

    Estudo de objetos em Javascript

---
+++ objetos.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome: "José",
+    idade: 30,
+    cpf:"4124121241",
+    email:"jose@gmail.com"
+}
+console.log(`Nome do cliente é ${cliente.nome} que tem ${cliente.idade} anos`);
