15d645a8dc24e91f77d892c0b08b1ea9a93e0ad2
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-11-01 11:10:24 -0300

    Adicionando campos novos no objeto

---
+++ objetos/adicionando-campos.js
@@ -0,0 +1,11 @@
+const cliente = {
+    nome: "José",
+    idade: 30,
+    cpf:"4124121241",
+    email:"jose@gmail.com"
+}
+console.log(cliente)
+
+cliente.telefone = "8899130131"
+
+console.log(cliente)
