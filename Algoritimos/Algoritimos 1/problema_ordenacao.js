e86a6b26478061ee1efac5aba4c4b140a18fa070
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-12-19 08:44:51 -0300

    Algoritimo para resolver um problema de Ordenação

--- Algoritimos/Algoritimos 1/problema_ordenacao.js
+++ Algoritimos/Algoritimos 1/problema_ordenacao.js
@@ -1,15 +1,21 @@
-const livros = require("./complemento_problema1.js")
-let menor = 0
-let maior = 0
-for(let i = 0; i < livros.length; i++){ 
-    if(livros[i].preco < livros[menor].preco){
-        menor = i
-    }
+let livros = require("./complemento_problema1.js")
+for(let i = 0; i < (livros.length); i++){
+    menor = menorvalor(livros, i)
+    precoAtual = livros[i];
+    precoBaixo = livros[menor]
+    livros[i] = precoBaixo
+    livros[menor] = precoAtual
+
 }
-for(let i = 0; i < livros.length; i++){ cd
-    if(livros[i].preco > livros[maior].preco){
-        maior = i
+function menorvalor(array, chave){
+    let x = chave
+    for(let i = chave; i < array.length; i++){ 
+        if(array[i].preco < array[x].preco){
+            x = i
+        }
     }
+    return x;
 }
-console.log(`O livro mais barato custa ${livros[menor].preco} e se chama: ${livros[menor].nome}`)
-console.log(`O livro mais caro custa ${livros[maior].preco} e se chama: ${livros[maior].nome}`)
+console.log(`Os livros ordenados do mais barato para o mais caro: 
+`)
+console.log(livros)
