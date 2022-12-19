104ab2627e8a547c0a5ad8fd0f904b53db084d3e
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-12-19 13:40:41 -0300

    Estudos sobre algoritimos de ordenação e suas complexidades

--- Algoritimos/Algoritimos 1/problema_ordenacao.js
+++ Algoritimos/Algoritimos 1/problema_ordenacao.js
@@ -1,21 +1,34 @@
-let livros = require("./complemento_problema1.js")
-for(let i = 0; i < (livros.length); i++){
-    menor = menorvalor(livros, i)
-    precoAtual = livros[i];
-    precoBaixo = livros[menor]
-    livros[i] = precoBaixo
-    livros[menor] = precoAtual
-
+const livros = require("./complemento_problema1.js")
+const trocaSelec = require("./trocaselec.js")
+const trocaInsert = require("./trocainsert.js")
+//Ordenando SelectionSort> Complexidade do Algoritimo: é um algoritimo quadradico, logo vai crescendo o 
+//numero de processo exponencialmente
+function ordenaprecoSelec(array){
+    let arrayclone = array.slice();
+    for(let i = 0; i < (arrayclone.length); i++){
+        trocaSelec(arrayclone, i)
+    }
+    return arrayclone;
 }
-function menorvalor(array, chave){
-    let x = chave
-    for(let i = chave; i < array.length; i++){ 
-        if(array[i].preco < array[x].preco){
-            x = i
+const livrosordenadosSelec = ordenaprecoSelec(livros)
+console.log(livros)
+console.log(`Os livros ordenados do mais barato para o mais caro com Select: 
+`)
+console.log(livrosordenadosSelec)
+//Ordenando InsertionSort > Complexidade do Algoritimo: Algoritimo quadratico
+function ordenaprecoInsert(array){
+    let arrayclone = array.slice()
+    for(let i = 0; i < (arrayclone.length); i++){
+        let atual = i;
+        while(atual > 0 && arrayclone[atual].preco < arrayclone[atual-1].preco){
+            trocaInsert(arrayclone, atual)
+            atual--;
         }
     }
-    return x;
+    return arrayclone
 }
-console.log(`Os livros ordenados do mais barato para o mais caro: 
+let livrosordenadosInsert = ordenaprecoInsert(livros);
+console.log(`Os livros ordenados do mais barato para o mais caro com Insert: 
 `)
-console.log(livros)
+console.log(livrosordenadosInsert)
+
