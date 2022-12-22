e5d1b79c66a8c1cf7d0e3aeba04660355ca7b565
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-12-22 13:53:46 -0300

    Progredindo

--- Node.js/src/cli.js
+++ Node.js/src/cli.js
@@ -1,10 +1,23 @@
-import pegaarquivo from "../index.js";
-
+import pegaarquivo from "./index.js";
+import chalk from "chalk";
+import fs from 'fs';
 const caminho = process.argv;
 
-async function processaTexto(caminho){
-    const resultado = await pegaarquivo(caminho[2]);
-    // return resultado;
-    console.log(resultado)
+async function processaTexto(argumentos){
+     const caminho = argumentos[2];
+
+    if(fs.lstatSync(caminho).isFile()){
+    const resultado = await pegaarquivo(argumentos[2]);
+    console.log(chalk.yellow('Os links: '), resultado) 
+    }else if (fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach((nomearquivo) => {
+            const lista = pegaarquivo(`${caminho}/${nomearquivo}`)
+            console.log(lista)
+        })
+        console.log(arquivos);
+    }
+    
+     
 }
 processaTexto(caminho)
