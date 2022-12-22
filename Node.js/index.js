a3541e33db2dd85115bff6d2080b96a3c5c7ce92
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-12-22 09:17:09 -0300

    Adicionando a função assicrona pegaarquivo, junto com tratamento de erros

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,2 +1,29 @@
-import chalk from "chalk";
-console.log(chalk.blue("Teste"))
+import fs from 'fs';//Biblioteca do Node.js para trabalhar com arquivos
+import chalk from "chalk";//Biblioteca para estilizar os resultados do console
+
+function trataerro(erro){
+    throw new Error(chalk.red(erro.code, "Arquivo não encontrado!"))//Throw joga os erros no console
+}
+// Usando then()
+/* function pegaarquivo(caminhoArquivo){
+    const encoding = 'utf-8';//Tipo de texto que o readfile vai receber
+    fs.promises.readFile(caminhoArquivo, encoding)
+    .then((texto) => console.log(chalk.blue(texto)))//Código assicrono retorna o resultado
+    .catch((erro) => trataerro(erro))//Código assicrono retornando o erro.
+} */
+
+// async/await (mais novo), mais simples de converter codigos sincronos em assicronos
+async function pegaarquivo(caminhoArquivo){
+    try{
+        const encoding = 'utf-8';//Tipo de texto que o readfile vai receber 
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding)
+        console.log(chalk.green(texto))
+    }catch(erro){
+        trataerro(erro)
+    }finally{
+        console.log(chalk.bgYellow("Operação finalizado"))
+    }
+}
+pegaarquivo("./texto.md");
+//1° Expresão Regular: \[[^[\]]*?\]
+//2°
