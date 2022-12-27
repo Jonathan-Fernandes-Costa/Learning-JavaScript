d384313713265d841aa0a49000ca7f685d6de42d
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-12-27 15:04:05 -0300

    Estudando o metodo antigo object.create e prototype

--- POO JavaScript/funções&protótipos.js
+++ POO JavaScript/funções&protótipos.js
@@ -5,18 +5,18 @@ const user = {
     nascimento: "03/04/2004",
     cargo: "estudante",
     ativo: true,
-    exibirInfos : function(){
+    exibirInfos: function () {
         console.log(this.nome, this.email)
     }
 }
-// const teste = user.exibirInfos
-// const teste2 = teste.bind(user)//Comando bind interliga a função teste com os dados de "user"
-// teste2()
+const teste = user.exibirInfos
+const teste2 = teste.bind(user)//Comando bind interliga a função teste com os dados de "user"
+teste2()
 const admin = {
     nome: "Jonathan",
     email: "jonathan@gmail.com",
     cargo: "admin",
-    criarCurso(){
+    criarCurso() {
         console.log("Curso criado")
     }
 }
