499089916dcbdfb467570fc7a51ecb3a1a5c27d1
Jonathan Fernandes <jonathanfernandes638@gmail.com>
2022-12-28 12:01:39 -0300

    Estudando herança de classes

--- POO JavaScript/heranças.js
+++ POO JavaScript/heranças.js
@@ -5,7 +5,28 @@ class Admin extends User {
     constructor(nome, email, nascimento, cargo = "admin", ativo = true){
         super(nome, email, nascimento, cargo, ativo)//Traz as propriedades de User
     }
+    criarTurma(nomedaTurma, vagas){
+        return `Turma: ${nomedaTurma}, quantidade de vagas: ${vagas}`
+    }
 }
 const admin = new Admin("Admin", "Admin@gmail.com", "02/03/2000")
 console.log(admin)
 console.log(admin.exibirInfos())
+console.log(admin.criarTurma("2° Ano", 20))
+
+class Professor extends User{
+    constructor(nome, emai, nascimento, cargo = "professor", ativo = true){
+        super(nome, emai, nascimento, cargo, ativo)
+    }
+
+    criarAula(horario, materia){
+        return `Aula de ${materia}, no horário: ${horario}, agendada`
+    }
+    darNota(nomeDoALuno, nota){
+        return `O aluno ${nomeDoALuno}, tirou ${nota} `
+    }
+}
+const professor = new Professor("Professor Mario", "mario@gmail.com", "03/02/1988")
+console.log(professor)
+console.log(professor.criarAula("10 PM", "Física"))
+console.log(professor.darNota("Joan", 10))
